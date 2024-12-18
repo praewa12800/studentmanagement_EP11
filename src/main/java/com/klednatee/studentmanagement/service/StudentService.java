f85e0008a3af9bc4f7e5438a2a83d45e270bab1e
praewa ainsuwan <praewa12800@gmail.com>
2024-12-18 08:57:06 +0700

    first commit

---
+++ src/main/java/com/klednatee/studentmanagement/service/StudentService.java
@@ -0,0 +1,11 @@
+package com.klednatee.studentmanagement.service;
+
+import com.klednatee.studentmanagement.dto.StudentDto;
+
+import java.util.List;
+
+public interface StudentService {
+    List<StudentDto> getAllStudents();
+
+    void creatstudent(StudentDto student);
+}
